68227760b0acb4ab55085379d07a415e53917819
Derrick <derrickpehjh@gmail.com>
2018-10-23 12:22:40 +0800

    removed unused validators in model class

--- src/com/project/model/User.java
+++ src/com/project/model/User.java
@@ -1,8 +1,5 @@
 package com.project.model;
 
-import com.opensymphony.xwork2.validator.annotations.RequiredStringValidator;
-import com.opensymphony.xwork2.validator.annotations.ValidatorType;
-
 public class User {
 
 	public User() {
@@ -16,7 +13,7 @@ public class User {
 		return userName;
 	}
 
-	@RequiredStringValidator(type = ValidatorType.FIELD, message = "UserName is a required field")
+	
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
@@ -24,7 +21,7 @@ public class User {
 	public String getPassword() {
 		return password;
 	}
-	@RequiredStringValidator(type = ValidatorType.FIELD, message = "Password is a required field")
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
